c881402318f16c8da0eaf93c8be851e7f65d9aac
Mukund Tiwari <mukundtiwari251103@gmail.com>
2025-07-17 14:20:30 +0530

    building the Admin Area and Audit Functionality

--- backend/src/main/java/com/secure/notes/services/UserService.java
+++ backend/src/main/java/com/secure/notes/services/UserService.java
@@ -1,6 +1,7 @@
 package com.secure.notes.services;
 
 import com.secure.notes.dtos.UserDTO;
+import com.secure.notes.models.Role;
 import com.secure.notes.models.User;
 
 import java.util.List;
@@ -13,4 +14,16 @@ public interface UserService {
     User findByUsername(String username);
 
     UserDTO getUserById(Long id);
+
+    void updatePassword(Long userId, String password);
+
+    void updateAccountLockStatus(Long userId, boolean lock);
+
+    public void updateAccountExpiryStatus(Long userId, boolean expire);
+
+    public void updateAccountEnabledStatus(Long userId, boolean enabled);
+
+    void updateCredentialsExpiryStatus(Long userId, boolean expire);
+
+    List<Role> getAllRoles();
 }
--- backend/src/main/java/com/secure/notes/services/impl/UserServiceImpl.java
+++ backend/src/main/java/com/secure/notes/services/impl/UserServiceImpl.java
@@ -8,7 +8,7 @@ import com.secure.notes.repositories.RoleRepository;
 import com.secure.notes.repositories.UserRepository;
 import com.secure.notes.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,6 +22,9 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     RoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     @Override
     public void updateUserRole(Long userId, String roleName) {
@@ -71,6 +74,58 @@ public class UserServiceImpl implements UserService {
     public User findByUsername(String username) {
         Optional<User> user = userRepository.findByUserName(username);
         return user.orElseThrow(() -> new RuntimeException("User not found with username: " + username));
+    }
+
+
+// UserServiceImpl.java
+
+    @Override
+    public void updatePassword(Long userId, String password) {
+        try {
+            User user = userRepository.findById(userId)
+                    .orElseThrow(() -> new RuntimeException("User not found"));
+            user.setPassword(passwordEncoder.encode(password));
+            userRepository.save(user);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to update password");
+        }
+    }
+
+    @Override
+    public void updateAccountLockStatus(Long userId, boolean lock) {
+        User user = userRepository.findById(userId).orElseThrow(()
+                -> new RuntimeException("User not found"));
+        user.setAccountNonLocked(!lock);
+        userRepository.save(user);
+    }
+
+    @Override
+    public void updateAccountExpiryStatus(Long userId, boolean expire) {
+        User user = userRepository.findById(userId).orElseThrow(()
+                -> new RuntimeException("User not found"));
+        user.setAccountNonExpired(!expire);
+        userRepository.save(user);
+    }
+
+    @Override
+    public void updateAccountEnabledStatus(Long userId, boolean enabled) {
+        User user = userRepository.findById(userId).orElseThrow(()
+                -> new RuntimeException("User not found"));
+        user.setEnabled(enabled);
+        userRepository.save(user);
+    }
+
+    @Override
+    public void updateCredentialsExpiryStatus(Long userId, boolean expire) {
+        User user = userRepository.findById(userId).orElseThrow(()
+                -> new RuntimeException("User not found"));
+        user.setCredentialsNonExpired(!expire);
+        userRepository.save(user);
+    }
+
+    @Override
+    public List<Role> getAllRoles() {
+        return roleRepository.findAll();
     }
 
 }
